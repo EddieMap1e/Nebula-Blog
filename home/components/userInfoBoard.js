42d5dee07fcc4551876f73c4eaa496dcf9fbe344
星垂月朦朧 <54883978+EddieMap1e@users.noreply.github.com>
2020-10-12 18:39:53 +0800

    修改了下显示

--- home/components/userInfoBoard.js
+++ home/components/userInfoBoard.js
@@ -108,10 +108,10 @@ Vue.component('userinfoboard',{
 			'<div style="padding:10px">'+
 				'<div @click="homeGo" @mouseover="indexOver" @mouseleave="indexLeave" :style="indexStyle"><i @mouseover="None" class="layui-icon layui-icon-home" style="color:navy"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;首页</div>'+
 				'<div @click="articleGo" @mouseover="indexOver" @mouseleave="indexLeave" :style="indexStyle"><i @mouseover="None" class="layui-icon layui-icon-align-left" style="color:navy"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;文章</div>'+
-				'<div @click="friendGo" @mouseover="indexOver" @mouseleave="indexLeave" :style="indexStyle"><i @mouseover="None" class="layui-icon layui-icon-friends" style="color:navy"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;好友</div>'+
-				'<div @click="albumGo" @mouseover="indexOver" @mouseleave="indexLeave" :style="indexStyle"><i @mouseover="None" class="layui-icon layui-icon-camera" style="color:navy"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;相册</div>'+
 				'<div @click="boardGo" @mouseover="indexOver" @mouseleave="indexLeave" :style="indexStyle"><i @mouseover="None" class="layui-icon layui-icon-form" style="color:navy"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;问答</div>'+
-				'<div @click="appGo" @mouseover="indexOver" @mouseleave="indexLeave" :style="indexStyle"><i @mouseover="None" class="layui-icon layui-icon-app" style="color:navy"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;应用</div>'+
+				'<div @click="friendGo" @mouseover="indexOver" @mouseleave="indexLeave" :style="indexStyle"><i @mouseover="None" class="layui-icon layui-icon-friends" style="color:navy"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;好友(正在开发)</div>'+
+				'<div @click="albumGo" @mouseover="indexOver" @mouseleave="indexLeave" :style="indexStyle"><i @mouseover="None" class="layui-icon layui-icon-camera" style="color:navy"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;相册(正在开发)</div>'+
+				'<div @click="appGo" @mouseover="indexOver" @mouseleave="indexLeave" :style="indexStyle"><i @mouseover="None" class="layui-icon layui-icon-app" style="color:navy"></i>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;应用(正在开发)</div>'+
 			'</div>'+
 			'<h4 style="font-size:11px;">友链</h4>'+
 			'<div style="padding:10px">'+
